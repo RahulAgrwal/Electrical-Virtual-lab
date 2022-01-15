6b84562488c07782037b28f7946cb60692825196
JKumar-J <kumarbjawale11122@gmail.com>
2022-01-15 11:08:15 +0530

    15.1.2022. 11.08

---
+++ exp2/pretest/questions.js
@@ -0,0 +1,61 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A junction where two (or) more than two network elements meet is known as ______________",
+    answer: "Node",
+    options: [
+      "Node",
+      "Branch",
+      "Loop",
+      "Mesh"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The basic laws for analyzing an electric circuit are :-",
+    answer: "Kirchhoff’s laws",
+    options: [
+      "Einstein’s theory",
+      "Newtons laws",
+      "Kirchhoff’s laws",
+      "Faradays laws"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In a circuit, the current is directly proportional to __________.",
+    answer: "Voltage",
+    options: [
+      "Temperature",
+      "Voltage",
+      "Resistance",
+      "Resistivity"
+    ]
+  },
+    {
+    numb: 4,
+    question: "All _____________ are loops but _______________ are not meshes",
+    answer: "Meshes,Loops",
+    options: [
+      "Loops, Meshes",
+      "Meshes,Loops",
+      "Branches, Loops",
+      "Nodes,Branches"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
