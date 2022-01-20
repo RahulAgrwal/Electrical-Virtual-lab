ef75e8247add7a72f520912fd5064e1c10733e36
Rahul Agarwal <rahulagarwal1126@gmail.com>
2022-01-20 23:17:04 +0530

    commit

--- exp2/postest/script.js
+++ exp2/postest/script.js
@@ -36,7 +36,7 @@ continue_btn.onclick = () => {
     startTimerLine(0); //calling startTimerLine function
 }
 
-let timeValue = 15;
+let timeValue = 60;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -51,7 +51,7 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = () => {
     quiz_box.classList.add("activeQuiz"); //show quiz box
     result_box.classList.remove("activeResult"); //hide result box
-    timeValue = 15;
+    timeValue = 60;
     que_count = 0;
     que_numb = 1;
     userScore = 0;
@@ -217,7 +217,7 @@ function startTimer(time) {
 }
 
 function startTimerLine(time) {
-    counterLine = setInterval(timer, 29);
+    counterLine = setInterval(timer, 109);
     function timer() {
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
