fe4833989b711b2bcef4a9fd072986a9741a11f6
Rahul <rahulagarwal1126@gmail.com>
2022-01-24 19:14:26 +0530

    24.01.2022 7.14 p.m.

--- exp3/simulator.js
+++ exp3/simulator.js
@@ -3,11 +3,13 @@ var rangeBullet = document.getElementById("rs-bullet");
 var counter=1;
 
 rangeSlider.addEventListener("input", showSliderValue, false);
-
 var A1 = [3,3.6,5.4,6.8,8,10,11.5,13,14.2,15.2];
 var V1 = [225,225,225,225,225,225,225,225,225,225];
 var A2 = [1.2,2.8,3.2,3.6,5.5,7,8.1,10.2,11,12.7];
 var V2 = [220,212,208,205,200,195,189,184,179,176];
+
+var tempindex=[1];
+ 
 
 function showSliderValue() {
   rangeBullet.innerHTML = rangeSlider.value;
@@ -27,6 +29,7 @@ function addtable() {
   document.getElementById("b"+counter).innerHTML=rangeSlider.value;
   document.getElementById("i"+counter).innerHTML=A2[index];
   document.getElementById("v"+counter).innerHTML=V2[index];
+  tempindex[counter-1]=rangeSlider.value;
   }
   else {
     alert("Only Six Readings are allowed.");
@@ -42,5 +45,65 @@ function reset() {
   document.getElementById("i"+i).innerHTML="";
   document.getElementById("v"+i).innerHTML="";
   }
+  tempindex=[];
+  var xValues=[];
+  var yValues=[];
+  
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        label:'Load Current',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "red",
+        borderColor: "red",
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: true},
+      scales: {
+        
+        yAxes: [{ticks: {min: 150, max:240}}],
+      }
+    }
+  });
+  document.getElementById("graph").innerHTML="";
 
+}
+function plot(){
+  tempindex.sort();
+  var xValues=[0];
+  var yValues=[0];
+  for(var i=0;i<tempindex.length;i++){
+    xValues[i]=A2[tempindex[i]-1];
+    yValues[i]=V2[tempindex[i]-1];
+  }
+ 
+
+  
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        label:'Voltage',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "red",
+        borderColor: "red",
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: true},
+      scales: {
+        
+        yAxes: [{ticks: {min: 150, max:240}}],
+      }
+    }
+  });
+  document.getElementById("graph").innerHTML="Graph Shows that whenever Load Current Increases Voltage Decreases.";
 }
